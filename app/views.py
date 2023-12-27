67e3ab2d7ed6f1499a219120ba695483290d6673
yuutarou15 <yhorie001@gmail.com>
2023-12-27 18:20:11 +0900

    line bot

--- app/views.py
+++ app/views.py
@@ -10,12 +10,13 @@ from django.conf import settings
 
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextSendMessage, TextMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 
 # LINE Messaging APIは、CHANNEL_ACCESS_TOKENとCHANNEL_SECRETが必要
 line_bot_api = LineBotApi(settings.CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(settings.CHANNEL_SECRET)
+
 
 
 class CallbackView(View):
@@ -43,8 +44,8 @@ class CallbackView(View):
     @staticmethod
     @handler.add(MessageEvent, message=TextMessage)
     def message_event(event):
-        reply = event.message.TextMessage
+        reply = event.message.text
         line_bot_api.reply_message(
             event.reply_token,
-            TextMessage(text=reply)
+            TextSendMessage(text=reply)
         )
