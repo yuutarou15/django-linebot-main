ea8a461a442345721a762797fb00812013b5fb1e
yuutarou15 <yhorie001@gmail.com>
2024-02-11 11:47:17 +0900

    コミット

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import CallbackView
 
 urlpatterns = [
-    path('callback/', CallbackView.as_view(), name='callback')
+    path('callback/', CallbackView.as_view(), name='callback'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,47 +1,48 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
+from django.http.response import HttpResponse
+# from django.http.response import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
+# from django.utils.decorators import method_decorator
+# from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import View
-from django.conf import settings
+# from django.conf import settings
 
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+# from linebot import LineBotApi, WebhookHandler
+# from linebot.exceptions import InvalidSignatureError, LineBotApiError
+# from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 
-# LINE Messaging APIは、CHANNEL_ACCESS_TOKENとCHANNEL_SECRETが必要
-line_bot_api = LineBotApi(settings.CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(settings.CHANNEL_SECRET)
+# # LINE Messaging APIは、CHANNEL_ACCESS_TOKENとCHANNEL_SECRETが必要
+# line_bot_api = LineBotApi(settings.CHANNEL_ACCESS_TOKEN)
+# handler = WebhookHandler(settings.CHANNEL_SECRET)
 
 
 class CallbackView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse('OK')
     
-    def post(self, request, *args, **kwargs):
-        signature = request.META['HTTP_X_LINE_SIGNATURE']
-        body = request.body.decode('utf-8')
+    # def post(self, request, *args, **kwargs):
+    #     signature = request.META['HTTP_X_LINE_SIGNATURE']
+    #     body = request.body.decode('utf-8')
 
-        try:
-            handler.handle(body, signature)
-        except InvalidSignatureError:
-            return HttpResponseBadRequest()
-        except LineBotApiError as e:
-            print(e)
-            return HttpResponseServerError()
-        return HttpResponse('OK')
+    #     try:
+    #         handler.handle(body, signature)
+    #     except InvalidSignatureError:
+    #         return HttpResponseBadRequest()
+    #     except LineBotApiError as e:
+    #         print(e)
+    #         return HttpResponseServerError()
+    #     return HttpResponse('OK')
     
-    @method_decorator(csrf_exempt)
-    def dispatch(self, *args, **kwargs):
-        return super(CallbackView, self).dispatch(*args, **kwargs)
+    # @method_decorator(csrf_exempt)
+    # def dispatch(self, *args, **kwargs):
+    #     return super(CallbackView, self).dispatch(*args, **kwargs)
     
 
-    @staticmethod
-    @handler.add(MessageEvent, message=TextMessage)
-    def message_event(event):
-        reply = event.message.text
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=reply)
-        )
+    # @staticmethod
+    # @handler.add(MessageEvent, message=TextMessage)
+    # def message_event(event):
+    #     reply = event.message.text
+    #     line_bot_api.reply_message(
+    #         event.reply_token,
+    #         TextSendMessage(text=reply)
+    #     )
