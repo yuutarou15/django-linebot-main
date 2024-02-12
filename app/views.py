d46a00b7868a4a11e335f53bfd870ade482cbf00
yuutarou15 <yhorie001@gmail.com>
2024-02-12 13:13:50 +0900

    テスト

--- app/views.py
+++ app/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
+from django.http.response import HttpResponse
 from django.views.generic.base import View
-from django.conf import settings
+# from django.http.response import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
+# from django.utils.decorators import method_decorator
+# from django.views.decorators.csrf import csrf_exempt
 
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+# from django.conf import settings
+
+# from linebot import LineBotApi, WebhookHandler
+# from linebot.exceptions import InvalidSignatureError, LineBotApiError
+# from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 
-# LINE Messaging APIは、CHANNEL_ACCESS_TOKENとCHANNEL_SECRETが必要
-line_bot_api = LineBotApi(settings.CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(settings.CHANNEL_SECRET)
+# # LINE Messaging APIは、CHANNEL_ACCESS_TOKENとCHANNEL_SECRETが必要
+# line_bot_api = LineBotApi(settings.CHANNEL_ACCESS_TOKEN)
+# handler = WebhookHandler(settings.CHANNEL_SECRET)
 
 
 class CallbackView(View):
